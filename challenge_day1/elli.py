5071605e1342e06b3faa7a8cd3cca0d87344facf
elhelin <126685104+elhelin@users.noreply.github.com>
2023-09-04 15:22:21 +0200

    Update elli.py

--- challenge_day1/elli.py
+++ challenge_day1/elli.py
@@ -3,3 +3,6 @@ def get_name():
 
 def paragraph3_elli():
     return "Yash and Felix were on time for the check-in. Diana and Elli were late."
+
+def paragraph7_elli():
+    return "Felix and Diana went to fill up theyr water bottles meanwhile Yash and Elli got sum free snacks at the entrance."
