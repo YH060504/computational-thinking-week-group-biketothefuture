8656b74f6ef64f9930c9d95c398e08c4494d971f
Yash <yashhajela01@gmail.com>
2023-09-04 15:46:52 +0200

    corrections

--- challenge_day1/diana.py
+++ challenge_day1/diana.py
@@ -2,10 +2,10 @@ def get_name():
   return "Diana"
 
 def paragraph2_diana():
-  return f"The campuz was filled with new faces and exitement. The air smelled of fresh coffee and Felix, Elli, Yash and Diana got some."
+  return f"The campus was filled with new faces and exitement. The air smelled of fresh coffee and Felix, Elli, Yash and Diana got some."
 
 def paragraph6_diana():
-  return f"After a verz nice introduction to the robot camp in the theater, everzone spread out to the sun. Felix, Yash, Elli and Diana joined other students and talked on the grass before another class started."
+  return f"After a very nice introduction to the robot camp in the theater, everzone spread out to the sun. Felix, Yash, Elli and Diana joined other students and talked on the grass before another class started."
 
-def paragraph6_diana():
-  return f"Little did they knoow about the coding obstacle they were about to face. Elli, Yash, Felix and Diana were frustrated."
+def paragraph10_diana():
+  return f"Little did they know about the coding obstacle they were about to face. Elli, Yash, Felix and Diana were frustrated."
