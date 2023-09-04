f2c824477b508d6aaf2be2c7abf094d838a18dc0
Yash <yashhajela01@gmail.com>
2023-09-04 15:33:34 +0200

    syntax

--- challenge_day1/yash.py
+++ challenge_day1/yash.py
@@ -1,13 +1,11 @@
 def get_name():
     return "Yash Hajela"
 
-if __name__ == "__main__":
-    name = get_name()
-    print(name)
-
 def paragraph_4_yash():
     return "The meting began with a few important announcements and continued with project-related work. Diana and Elli filled the team charter, while Felix and Yash reserched about the client."
 
 def paragraph_8_yash():
     return "Felix, Elli, Diana and Yash decided to work on the group chalenge after taking a lunch brek."
 
+def paragraph_12_yash():
+    return "Thus, Felix, Elli, Diana and Yash successfully avoded a mental breakdown and compleeted the task."
