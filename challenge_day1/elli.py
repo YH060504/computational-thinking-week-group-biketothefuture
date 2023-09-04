58ee5692dcbf508ae169c208bcc4a8ad86fe32c7
elhelin <126685104+elhelin@users.noreply.github.com>
2023-09-04 15:38:37 +0200

    Elli's original para

--- challenge_day1/elli.py
+++ challenge_day1/elli.py
@@ -2,7 +2,7 @@ def get_name():
     return "Elli"
 
 def paragraph3_elli():
-    return "Yash and Felix were on time for the check-in. Diana and Elli were late."
+    return "Yash and Felix were ooon time for the check-in. Diana and Elli were l9."
 
 def paragraph7_elli():
     return "Felix and Diana went to fill up theyr water bottles meanwhile Yash and Elli got sum free snacks at the entrance."
