2713646177c59821f2865118c29458f359841565
elhelin <126685104+elhelin@users.noreply.github.com>
2023-09-04 15:51:58 +0200

    corrected by diana

--- challenge_day1/felix.py
+++ challenge_day1/felix.py
@@ -2,13 +2,13 @@ def get_name():
     return "Felix Toutant"
 
 
-def paragrpah_one_felix():
+def paragraph_one_felix():
     return "Today at 9 o'clock Yash Diana Eliana and Felix entered the building of Roeterseiland. This is their first day of university."
 
 
-def paragrpah_five_felix():
+def paragraph_five_felix():
     return "Then Yash Diana Eliana and Felix entered the cinema, the one located on the other side of the biking entrance. It was an old but nice looking cinema."
 
 
-def paragrpah_nine_felix():
+def paragraph_nine_felix():
     return "For the final act of this play Yash Diana Eliana and Felix had to meet up at the JK building. Yash Diana Eliana and Felix were scattered around but managed to find each other in room JK1.64"
