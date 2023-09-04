c813164851e61dc6ce0587b401ff61655a58464f
elhelin <126685104+elhelin@users.noreply.github.com>
2023-09-04 15:41:37 +0200

    corrected by diana

--- challenge_day1/felix.py
+++ challenge_day1/felix.py
@@ -7,8 +7,8 @@ def paragrpah_one_felix():
 
 
 def paragrpah_five_felix():
-    return "Then Yash Diana Eliana and Felix entered the cinema, the one locted on the other   of the biking entrance. It was an ld but nice looking cinema."
+    return "Then Yash Diana Eliana and Felix entered the cinema, the one located on the other   of the biking entrance. It was an ld but nice looking cinema."
 
 
 def paragrpah_nine_felix():
-    return "For the fnal act of this play Yash Diana Eliana and Felix had to meet up at the JK building. Yash Diana Eliana and Felix was scattered around but menaged to find each other in room jk1.64"
+    return "For the final act of this play Yash Diana Eliana and Felix had to meet up at the JK building. Yash Diana Eliana and Felix was scattered around but menaged to find each other in room jk1.64"
