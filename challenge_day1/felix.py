276260b478021ba4f07ae793f0fa972050facf42
FelixToutant-UVA <126685041+FelixToutant-UVA@users.noreply.github.com>
2023-09-04 15:31:49 +0200

    Added my paragrahs
    git commit -m

--- challenge_day1/felix.py
+++ challenge_day1/felix.py
@@ -1,3 +1,14 @@
 def get_name():
     return "Felix Toutant"
 
+
+def paragrpah_one_felix():
+    return "Today ad 9 o'clock Yash Diana Eliana and Felix entred the  of Roeterseiland. This is their first day of uni."
+
+
+def paragrpah_two_felix():
+    return "Then Yash Diana Eliana and Felix entered the cinema, the one locted on the other   of the biking entrance. It was an ld but nice looking cinema."
+
+
+def paragrpah_three_felix():
+    return "For the fnal act of this play Yash Diana Eliana and Felix had to meet up at the JK building. Yash Diana Eliana and Felix was scattered around but menaged to find each other in room jk1.64"
--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -7,9 +7,19 @@ e = elli.get_name()
 f = felix.get_name()
 y = yash.get_name()
 
-def team_names ():
-  return f"This is Team Bike to the Future. We are: {f}, {e}, {y}"
+a = felix.paragrpah_one_felix
+
+
+d = felix.paragrpah_two_felix
+
+
+g = felix.paragrpah_three_felix
+
+
+def team_names():
+    return f"This is Team Bike to the Future. We are: {f}, {e}, {y}"
+
 
 if __name__ == "__main__":
-  team_name = team_names()
-  print(team_name)
+    team_name = team_names()
+    print(team_name)
