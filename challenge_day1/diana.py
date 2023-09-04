1349e0df3c26f8139dea2b183e5154f07266cc9a
elhelin <126685104+elhelin@users.noreply.github.com>
2023-09-04 15:18:44 +0200

    Update diana.py

--- challenge_day1/diana.py
+++ challenge_day1/diana.py
@@ -3,3 +3,6 @@ def get_name():
 
 def paragraph2_diana():
   return f"The campus was filled with new faces and exitement. The air smelled of fresh free coffee."
+
+def paragraph6_diana():
+  return f"After a verz nice introduction to the robot camp in the theater, everzone spread out to the sun. Elli and Diana joined other students and talked on the grass before another class started."
