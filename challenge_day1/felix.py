63a1a597f3b9b34aa3e2fca8ca8ee64af37322f8
FelixToutant-UVA <126685041+FelixToutant-UVA@users.noreply.github.com>
2023-09-04 15:39:59 +0200

    Update felix.py

--- challenge_day1/felix.py
+++ challenge_day1/felix.py
@@ -6,9 +6,9 @@ def paragrpah_one_felix():
     return "Today ad 9 o'clock Yash Diana Eliana and Felix entred the  of Roeterseiland. This is their first day of uni."
 
 
-def paragrpah_two_felix():
+def paragrpah_five_felix():
     return "Then Yash Diana Eliana and Felix entered the cinema, the one locted on the other   of the biking entrance. It was an ld but nice looking cinema."
 
 
-def paragrpah_three_felix():
+def paragrpah_nine_felix():
     return "For the fnal act of this play Yash Diana Eliana and Felix had to meet up at the JK building. Yash Diana Eliana and Felix was scattered around but menaged to find each other in room jk1.64"
