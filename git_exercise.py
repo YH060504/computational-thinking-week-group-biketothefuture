11d00d294df64dc85f9d76d2a728671f1f2f9405
elhelin <126685104+elhelin@users.noreply.github.com>
2023-09-04 14:21:24 +0200

    Update git_exercise.py

--- git_exercise.py
+++ git_exercise.py
@@ -1,1 +1,8 @@
+from elli import function_for_name as name_team_member_1
+from yash import get_name as name_team_member_2
 
+def team_names():
+  print("This is Team Bike to the Future. We are: team_names")
+
+team_member1_name = get_name()
+team
