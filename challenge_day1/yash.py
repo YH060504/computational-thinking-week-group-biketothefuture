6f3710a09ccecc16c2ee680f8f968ccb85b5056a
Yash <yashhajela01@gmail.com>
2023-09-04 15:32:39 +0200

    story

--- challenge_day1/yash.py
+++ challenge_day1/yash.py
@@ -4,3 +4,10 @@ def get_name():
 if __name__ == "__main__":
     name = get_name()
     print(name)
+
+def paragraph_4_yash():
+    return "The meting began with a few important announcements and continued with project-related work. Diana and Elli filled the team charter, while Felix and Yash reserched about the client."
+
+def paragraph_8_yash():
+    return "Felix, Elli, Diana and Yash decided to work on the group chalenge after taking a lunch brek."
+
