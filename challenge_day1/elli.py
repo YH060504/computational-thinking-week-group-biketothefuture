cc078b6dbbc0696afee984e7aac8107345359597
Yash <yashhajela01@gmail.com>
2023-09-04 15:49:24 +0200

    corrections

--- challenge_day1/elli.py
+++ challenge_day1/elli.py
@@ -2,10 +2,10 @@ def get_name():
     return "Elli"
 
 def paragraph3_elli():
-    return "Yash and Felix were ooon time for the check-in. Diana and Elli were l9."
+    return "Yash and Felix were on time for the check-in. Diana and Elli were late."
 
 def paragraph7_elli():
-    return "Felix and Diana went to fill up theyr water bottles meanwhile Yash and Elli got sum free snacks at the entrance."
+    return "Felix and Diana went to fill up their water bottles meanwhile Yash and Elli got some free snacks at the entrance."
 
 def paragraph11_elli():
-    return "Deana and Yash seeked for heulp. Felix and Elli waited in the room for the heulp to arrive."
+    return "Diana and Yash seeked for help. Felix and Elli waited in the room for the help to arrive."
