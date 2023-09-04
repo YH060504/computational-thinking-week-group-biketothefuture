65a2370e895e17cc825b52b21df186ca089cce3a
elhelin <126685104+elhelin@users.noreply.github.com>
2023-09-04 14:27:45 +0200

    Update git_exercise.py

--- git_exercise.py
+++ git_exercise.py
@@ -1,8 +1,17 @@
-from elli import function_for_name as name_team_member_1
-from yash import get_name as name_team_member_2
+from elli import get_name as get_name_team_member_1
+from yash import get_name as get_name_team_member_2
+from felix import get_name as get_name_team_member_3
 
 def team_names():
-  print("This is Team Bike to the Future. We are: team_names")
+  print("This is Team Bike to the Future. We are:")
 
-team_member1_name = get_name()
-team
+name_team_member_1 = get_name_team_member_1()
+name_team_member_2 = get_name_team_member_2()
+name_team_member_3 = get_name_team_member_3()
+
+print(f" - {name_team_member_1}")
+print(f" - {name_team_member_2}")
+print(f" - {name_team_member_3}")
+
+if __name__ == "__main__":
+  team_names()
