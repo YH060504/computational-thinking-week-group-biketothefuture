901fc96594937eb571dbf264f16d520f28b8efc9
elhelin <126685104+elhelin@users.noreply.github.com>
2023-09-04 15:16:35 +0200

    Update git_exercise.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -2,13 +2,15 @@
 import felix
 import elli
 import yash
+import diana
 
 e = elli.get_name()
 f = felix.get_name()
 y = yash.get_name()
+d = diana.get_name()
 
 def team_names ():
-  return f"This is Team Bike to the Future. We are: {f}, {e}, {y}"
+  return f"This is Team Bike to the Future. We are: {f}, {e}, {y}, {d}"
 
 if __name__ == "__main__":
   team_name = team_names()
