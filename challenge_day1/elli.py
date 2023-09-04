cceccd1b51824a5bbe929aadb5556856d12ce36a
elhelin <eliana.helin@gmail.com>
2023-09-04 16:43:24 +0200

    Corrected name mistake

--- challenge_day1/elli.py
+++ challenge_day1/elli.py
@@ -1,11 +1,19 @@
+import felix
+import yash
+import diana
+
+f = felix.get_name()
+y = yash.get_name()
+d = diana.get_name()
+
 def get_name():
     return "Elli"
 
 def paragraph3_elli():
-    return "Yash and Felix were on time for the check-in. Diana and Elli were late."
+    return f"{y} and {f} were on time for the check-in. {d} and Elli were late."
 
 def paragraph7_elli():
-    return "Felix and Diana went to fill up their water bottles meanwhile Yash and Elli got some free snacks at the entrance."
+    return f"{f} and {d} went to fill up their water bottles meanwhile {y} and Elli got some free snacks at the entrance."
 
 def paragraph11_elli():
-    return "Diana and Yash seeked for help. Felix and Elli waited in the room for the help to arrive."
+    return f"{d} and {y} seeked for help. {f} and Elli waited in the room for the help to arrive."
