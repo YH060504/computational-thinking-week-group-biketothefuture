e2771c9099ce83ace3f0656005fc77b18ad57437
FelixToutant-UVA <126685041+FelixToutant-UVA@users.noreply.github.com>
2023-09-06 13:39:11 +0200

    changed

--- challenge_day3/station4.py
+++ challenge_day3/station4.py
@@ -1,12 +1,12 @@
 def solution_station_4(x):
     output = "True"
-    
+
     if x == 1:
         output = "False"
     elif x <= 3:
         output = "True"
     elif x % 2 == 0 or x % 3 == 0:
-        output =  "False"
+        output = "False"
     else:
         i = 5
         while i * i <= x:
@@ -14,8 +14,19 @@ def solution_station_4(x):
                 output = "False"
                 break
             i += 2
-        
+
     return output
 
+
 x = 61
-print (solution_station_4(x))
+print(solution_station_4(x))
+
+
+def solution_station_6(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+
+print(solution_station_6(50))
