3004cc2378b4bf093ec385c31b399007530259af
FelixToutant-UVA <126685041+FelixToutant-UVA@users.noreply.github.com>
2023-09-06 15:19:14 +0200

    last changes

--- challenge_day3/station3.py
+++ challenge_day3/station3.py
@@ -1,13 +1,14 @@
 
 # X = station number
 def solution_station_3(number):
-  if number % 3 == 0:
-    output = "True"
-  else:
-    output = "False"
-  return output
+    if number % 3 == 0:
+        output = True
+    else:
+        output = False
+    return output
+
 
 number = 57217
-print (solution_station_3(number))
+print(solution_station_3(number))
 number = 75212
-print (solution_station_3(number))
+print(solution_station_3(number))
--- challenge_day3/station4.py
+++ challenge_day3/station4.py
@@ -1,17 +1,17 @@
 def solution_station_4(x):
-    output = "True"
+    output = True
 
     if x == 1:
-        output = "False"
+        output = False
     elif x <= 3:
-        output = "True"
+        output = True
     elif x % 2 == 0 or x % 3 == 0:
-        output = "False"
+        output = False
     else:
         i = 5
         while i * i <= x:
             if x % i == 0:
-                output = "False"
+                output = False
                 break
             i += 2
 
