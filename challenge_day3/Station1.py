d80c48893ffcc6847d5eaa57283140e64ae01551
Yash <yashhajela01@gmail.com>
2023-09-06 13:27:15 +0200

    Update Station1.py

--- challenge_day3/Station1.py
+++ challenge_day3/Station1.py
@@ -1,1 +1,15 @@
+def solution_station_1(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        return solution_station_1(n - 1) + solution_station_1(n - 2)
+
+# Example usage:
+n = int(input("Enter the value of n: "))
+result = solution_station_1(n)
+print(f"The {n}th term of the Fibonacci sequence is: {result}")
 
