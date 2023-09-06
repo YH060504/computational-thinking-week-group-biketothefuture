c74f112855455e68f9cdc537ff1d0d5e69e496a5
FelixToutant-UVA <126685041+FelixToutant-UVA@users.noreply.github.com>
2023-09-06 11:56:53 +0200

    Update Station2.py

--- challenge_day3/Station2.py
+++ challenge_day3/Station2.py
@@ -1,10 +1,13 @@
 import datetime
 
-date_obj = datetime.date(2023, 1, 9)
 
-days = ["月曜日", "火曜日", "水曜日",
-        "木曜日", "金曜日", "土曜日", "日曜日"]
+def solution_station_2(year, month, day):
+    days = ["月曜日", "火曜日", "水曜日",
+            "木曜日", "金曜日", "土曜日", "日曜日"]
 
-day_name = days[date_obj.weekday()]
+    date_obj = datetime.date(year, month, day)
 
-print(day_name)
+    return days[date_obj.weekday()]
+
+
+print(solution_station_2(2023, 3, 26))
