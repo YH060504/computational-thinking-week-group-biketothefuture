a9340332415164c38d43e1d15349f4dddc5c4d5a
FelixToutant-UVA <126685041+FelixToutant-UVA@users.noreply.github.com>
2023-09-06 15:15:15 +0200

    hello

--- challenge_day3/station2.py
+++ challenge_day3/station2.py
@@ -1,13 +1,15 @@
 import datetime
 
 
-def solution_station_2(year, month, day):
+def solution_station_2(date_string):
     days = ["月曜日", "火曜日", "水曜日",
             "木曜日", "金曜日", "土曜日", "日曜日"]
 
+    year, month, day = map(int, date_string.split('-'))
     date_obj = datetime.date(year, month, day)
 
     return days[date_obj.weekday()]
 
 
-print(solution_station_2(2023, 3, 26))
+# Test the function
+print(solution_station_2('2024-07-10'))
