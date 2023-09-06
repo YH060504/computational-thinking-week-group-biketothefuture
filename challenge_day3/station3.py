9990b00e9f760e16d669bd94335f792c4329cacb
diananina <127301256+diananina@users.noreply.github.com>
2023-09-06 11:03:12 +0200

    updated

--- challenge_day3/station3.py
+++ challenge_day3/station3.py
@@ -1,10 +1,13 @@
 
 # X = station number
 def solution_station_3(number):
-  if number % 3:
+  if number % 3 == 0:
     output = "True"
   else:
     output = "False"
+  return output
 
-if number == 57217:
-  print (output)
+number = 57217
+print (solution_station_3(number))
+number = 75212
+print (solution_station_3(number))
