432cf70c7783e68cfc0c6926977fc85e88ceaa66
elhelin <eliana.helin@gmail.com>
2023-09-06 13:45:26 +0200

    Solution

--- challenge_day3/Station5.py
+++ challenge_day3/Station5.py
@@ -1,12 +1,24 @@
-group1 =
-group2 =
-group3 =
-group4 =
-group5 = ["Kian", "Gonzalo", "Sahir", "Teun", "Tom"]
-group6 = ["Angeline", "Matas", "Raven", "Caleb", "Paulina", "Angelica"]
-group7 = ["Mate", "Martyna", "Vincent", "Eryk", "Emma"]
-group8 = ["Sanne", "Beatris", "Sally", "Alyssa", "Caio", "Hielke"]
-group9 = 'Atlas Yash Eliana Felix Diana'
-group10 = 'Charlie Andrey Akanksha Max Hugo Al-fatihi'
-group11 = 'Ioanna Vanessa Ella Alicja Cristian '
-group12 = 'Karolina Rachna Luca Alexia Yuyue Jelle'
+def solution_station_5(name):
+    groups = {
+        1: ["Charlie", "Andrey", "Akanksha", "Hugo", "Al-fatihi"],
+        2: ["Maria", "Celia", "Gur", "Carine", "Sabrina"],
+        3: ["Elisa", "Dmitry", "Sari", "Martyna", "Jesper", "Kavin"],
+        4: ["Kelt", "Sebastian", "Quanpu", "Ruben", "Sofia"],
+        5: ["Kian", "Gonzalo", "Sahir", "Teun", "Tom"],
+        6: ["Angeline", "Matas", "Raven", "Caleb", "Paulina", "Angelica"],
+        7: ["Mate", "Martyna", "Vincent", "Eryk", "Emma"],
+        8: ["Sanne", "Beatris", "Sally", "Alyssa", "Caio", "Hielke"],
+        9: ['Atlas', 'Yash', 'Eliana', 'Felix', 'Diana'],
+        10: ['Charlie', 'Andrey', 'Akanksha', 'Max', 'Hugo', 'Al-fatihi'],
+        11: ['Ioanna', 'Vanessa', 'Ella', 'Alicja', 'Cristian'],
+        12: ['Karolina', 'Rachna', 'Luca', 'Alexia', 'Yuyue', 'Jelle']
+    }
+
+    for group, names_in_group in groups.items():
+        if name in names_in_group:
+            return group
+        
+
+name_to_find = "Sofia"
+result = solution_station_5(name_to_find)
+print(result)
