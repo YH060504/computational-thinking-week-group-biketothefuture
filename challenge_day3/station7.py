90fcfee9bc82cf628642b767702bb9f91f38d065
diananina <127301256+diananina@users.noreply.github.com>
2023-09-06 12:10:58 +0200

    Update station7.py

--- challenge_day3/station7.py
+++ challenge_day3/station7.py
@@ -1,4 +1,5 @@
 
+
 a = 3
 b = -1
 c = 4
@@ -6,4 +7,21 @@ d = 7
 e = 0.5
 
 def solution_station_7(a, b, c, d, e):
-  if 
+  if expression == "a+b":
+    return a + b
+  elif expression == "a+c":
+    return a + c
+  elif expression == "a+d":
+    return a + d
+  elif expression == "b+a":
+    return a
+  elif expression == "b+c":
+    return a
+  elif expression == "b+d":
+    return a
+  elif expression == "c+a":
+    return a
+  elif expression == "c+b":
+    return a
+  elif expression == "c+d":
+    return a
